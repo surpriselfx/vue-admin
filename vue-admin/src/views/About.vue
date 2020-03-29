debed6ce715bb3fc9380b95e1b9628650f72cb05
surprise12 <1607416911@qq.com>
2020-03-29 11:34:03 +0800

    第一次提交测试

---
+++ vue-admin/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>这是我的第一个vue网页 哈哈哈哈哈</h1>
+  </div>
+</template>
---
+++ vue-admin/src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="login">
+        外层必须有一层来包裹  不一定是div   别的也可以
+    </div>
+</template>
+<script></script>
+<style lang="scss" scoped>  //如果不加scoped  则下面的饿样式会对全局有效  加了scoped则下面的饿样式只对该页面有效
+    #login {
+        height: 100vh;   //100vh:表示占可视区的100%； 50vh表示占可视区的一半
+        background-color: #cccccc;
+    }
+</style>
